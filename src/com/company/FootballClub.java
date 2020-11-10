6a2119cd0eb17195bc1de2f3333b9bf588b29dd9
Mahfooz <umahfoosahamed@gmail.com>
2020-11-10 22:33:02 +0530

    first commit

---
+++ src/com/company/FootballClub.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class FootballClub extends SportsClub {
+	private int noOfWin;
+	private int noOfDraw;
+	private int noOfDefeat;
+	private int noOfGoal;
+
+}
---
+++ src/com/company/SportsClub.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class SportsClub {
+	private String nameOfClub;
+	private String locationOfCLub;
+	private int contactNo;
+}
